9971740acde0182be0e22a3b6664f6bf6f3f115a
Toru Higuruma <shizuruma@outlook.jp>
2017-09-19 02:02:14 +0900

    Change source_url into my repo

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -33,12 +33,12 @@ module Mastodon
     end
 
     def source_base_url
-      'https://github.com/tootsuite/mastodon'
+      'https://github.com/neofuji/mastodon'
     end
 
     # specify git tag or commit hash here
     def source_tag
-      nil
+      'highlight'
     end
 
     def source_url
